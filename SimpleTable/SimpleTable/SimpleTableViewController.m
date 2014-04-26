c3c484965aeaca356f3cd773a6f4cc1214c8a9b1
adiagarwalla <aa4@princeton.edu>
2014-04-26 15:06:25 -0400

    Basic rows show up

--- SimpleTable/SimpleTable/SimpleTableViewController.m
+++ SimpleTable/SimpleTable/SimpleTableViewController.m
@@ -13,17 +13,40 @@
 @end
 
 @implementation SimpleTableViewController
+{
+    NSArray *tableData;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    tableData = [NSArray arrayWithObjects:@"A", @"B", @"C", @"D", nil];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [tableData count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableItem";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
+    return cell;
 }
 
 @end
